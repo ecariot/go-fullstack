52a0270296f1e021a5a7327cf3c51a839ebfb47c
Emma Cariot <emm.cariot@gmail.com>
2021-08-15 10:05:39 +0200

    Système authentification okkkk

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const userRoutes = require('./routes/user');
+const userRoutes = require('./routes/users');
 const stuffRoutes = require('./routes/stuff');
 const app = express();
 
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
 const Thing = require('../models/thing');
-
-// in routes/stuff.js
 
 const stuffCtrl = require('../controllers/stuff');
 
-router.get('/', stuffCtrl.getAllStuff);
-router.post('/', stuffCtrl.createThing);
-router.get('/:id', stuffCtrl.getOneThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
 
